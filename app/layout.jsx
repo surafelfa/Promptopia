ddaea15f3ef766ff5725a19ae8cc2784308f273c
surafelfa <surafelfa@gmail.com>
2023-05-11 14:45:55 +0300

    auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import "@styles/global.css";
 
-const RootLayout = () => {
-  return (
-    <html lang="en">
-      <body></body>
-    </html>
-  );
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
 };
+
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>
+      <Provider>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
 
 export default RootLayout;
